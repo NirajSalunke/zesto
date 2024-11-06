437aeccc4bf33a41a9e624972668950964856282
NirajSalunke <154681227+NirajSalunke@users.noreply.github.com>
2024-11-06 23:16:53 +0530

    Time-ZOne issues

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -2,21 +2,24 @@ import MeetingTypeList from "@/components/MeetingTypeList";
 const Home = () => {
   const date = new Date();
   // ok ok solution hai ye wala
-  const time = date.toLocaleTimeString("en-US", {
+  const time = new Intl.DateTimeFormat("en-US", {
     hour: "2-digit",
     minute: "2-digit",
-  });
+    hour12: true,
+    timeZone: "Asia/Kolkata",
+  }).format(date);
 
   // customly made hai ye wala niche wala
   // const time = `${
   //   date.getHours() > 12 ? (date.getHours() - 12).toString() : date.getHours()
   // }:${date.getMinutes() < 10 ? "0" : ""}${date.getMinutes()}`;
 
-  const day = date.toLocaleDateString("en-AU", {
+  const day = date.toLocaleDateString("en-US", {
     weekday: "long",
     month: "long",
     day: "numeric",
     year: "numeric",
+    timeZone: "Asia/Kolkata",
   });
   // console.log(`${time} , ${day}`);
 
