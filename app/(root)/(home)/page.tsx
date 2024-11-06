33d9e50db59eed0a86fcf7c7a0b8275ffa1b3718
NirajSalunke <154681227+NirajSalunke@users.noreply.github.com>
2024-11-06 22:58:25 +0530

    works

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -22,7 +22,7 @@ const Home = () => {
 
   return (
     <section className="flex size-full flex-col gap-10 text-white">
-      <div className="h-[300px] w-full rounded-[20px] bg-hero  bg-cover">
+      <div className="h-[270px] w-full rounded-[20px] bg-hero  bg-cover">
         <div className="flex h-full flex-col justify-between max-md:px-5 max-md:py-8 lg:p-11">
           <h1 className="glassmorphism max-w-[270px] rounded py-2 text-center text-base font-normal">
             Let&apos;s get started
