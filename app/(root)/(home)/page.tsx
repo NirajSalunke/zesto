1d22326757a69c347da21ec5c0c33243c8a4ab51
NirajSalunke <154681227+NirajSalunke@users.noreply.github.com>
2024-11-06 22:51:04 +0530

    lint issues

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -25,7 +25,7 @@ const Home = () => {
       <div className="h-[300px] w-full rounded-[20px] bg-hero  bg-cover">
         <div className="flex h-full flex-col justify-between max-md:px-5 max-md:py-8 lg:p-11">
           <h1 className="glassmorphism max-w-[270px] rounded py-2 text-center text-base font-normal">
-            Let's get started
+            Let&apos;s get started
           </h1>
           <div className="flex flex-col gap-2 ">
             <div>
